5f39da39583d636f500feedc74242a7ed35ad4b2
natalia646 <nataxash46@gmail.com>
2023-09-29 13:44:46 +0300

    ideally work faq

--- src/components/Questions/Item.js
+++ src/components/Questions/Item.js
@@ -1,10 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import style from "./Ouestions.module.scss";
 import more from "../assets/more.svg";
-import less from "../assets/less.svg";
 
 const Item = ({ id, item }) => {
-  const refItem = useRef(null);
   const [active, setActeve] = useState(null);
 
   const clickQuestion = (id) => {
@@ -23,11 +21,7 @@ const Item = ({ id, item }) => {
         onClick={() => clickQuestion(id)}
       >
         <p>{item.q}</p>
-        <img
-          className={style.svg}
-          src={more}
-          alt="more"
-        ></img>
+        <img className={style.svg} src={more} alt="more"></img>
       </div>
       <div className={`${style.answer} ${id === active ? style.a_open : ""}`}>
         {item.a}
