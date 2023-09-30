0666f166a39f5a585c402a74c26b34d90b3a404b
natalia646 <nataxash46@gmail.com>
2023-09-30 18:59:55 +0300

    active links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
-import style from './Header.module.scss'
-import Download from '../ButtonDownload/Download';
-
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import style from "./Header.module.scss";
+import Download from "../ButtonDownload/Download";
 
 const Header = () => {
+  const links = ["/", "/", "/", "/"];
+  const nameLink = ["Про додаток", "Відгуки", "Умови користування", "Контакти"];
+
+  const [activeLink, setActiveLink] = useState(0);
 
   return (
     <div className={style.header_container}>
-      <Link ><img src={logo} alt ='logo'></img></Link>
+      <Link>
+        <img src={logo} alt="logo"></img>
+      </Link>
       <div className={style.links}>
-          <Link className={style.link} to = "/">Про додаток</Link>
-          <Link className={style.link} to = "/">Відгуки</Link>
-          <Link className={style.link} to = "/">Умови користування</Link>
-          <Link className={style.link} to = "/">Контакти</Link>
-          <Download className={style.button_download}/>
-      
+        {links.map((item, i) => {
+          return (
+            <Link
+              onClick={() => setActiveLink(i)}
+              className={`${style.link} ${i === activeLink ? style.active : ""} `}
+              to={item}
+            >
+              {nameLink[i]}
+            </Link>
+            
+          );
+        })}
+        <Download className={style.button_download} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
