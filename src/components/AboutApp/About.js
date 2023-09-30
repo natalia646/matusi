e6b8a089946730740b927d0992c25f5550409487
natalia646 <nataxash46@gmail.com>
2023-09-30 09:53:32 +0300

    ideally about component

--- src/components/AboutApp/About.js
+++ src/components/AboutApp/About.js
@@ -1,28 +1,41 @@
 import React from "react";
 import style from "./About.module.scss";
-import location from '../assets/location.svg'
+import location from "../assets/location.svg";
+import group from "../assets/group.svg";
+import purse from "../assets/purse.svg";
 
 const About = () => {
+  const img = [location, group, purse];
+  const h3 = ["Обирай локацію", "Створюй групи", "Працюй спокійно"];
+  const p = [
+    "знаходь поблизу себе інших мам, що також потребують догляду за своєю дитиною",
+    "шукай чи створюй групи для вирішення спільної проблеми за бажаним місцем проживання, віком дитини, розкладом мами",
+    "та допомагай іншим мамам отримати таку можливість, завдяки догляду за вашими дітьми",
+  ];
+
   return (
     <div className={style.about_container}>
-      <h2>Як це працює?</h2>
-      <p>
-        Вам потрібно йти на роботу? Ви шукаєте дитячий садок для своїх дітей?
-        Більшість послуг дитячого садка недоступні або занадто дорогі для вас?
-        Ми допоможемо знайти поруч мам з такими ж проблемами!{" "}
-      </p>
+      <div className={style.desc}>
+        <h2>Як це працює?</h2>
+        <p>
+          Вам потрібно йти на роботу? Ви шукаєте дитячий садок для своїх дітей?
+          <br />
+          Більшість послуг дитячого садка недоступні або занадто дорогі для вас?
+          <br />
+          Ми допоможемо знайти поруч мам з такими ж проблемами!
+        </p>
+      </div>
+
       <div className={style.boxes}>
-        <div className={style.box}>
-          <img src={location} alt='svg'></img>
-          <h3>Обирай локацію</h3>
-          <p>знаходь поблизу себе інших мам, що також потребують догляду за своєю дитиною</p>
-        </div>
-        <div className={style.box}>
-          <h3>Створюй групи</h3>
-        </div>
-        <div className={style.box}>
-          <h3>Працюй спокійно</h3>
-        </div>
+        {img.map((item, i) => {
+          return (
+            <article key={i} className={style.box}>
+              <img src={item} alt="svg"></img>
+              <h3>{h3[i]}</h3>
+              <p>{p[i]}</p>
+            </article>
+          );
+        })}
       </div>
     </div>
   );
