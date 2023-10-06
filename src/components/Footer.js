c3abc372a20e9efdc53f6c5b8b9159dcbb3ffd4d
natalia646 <nataxash46@gmail.com>
2023-10-06 19:04:18 +0300

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,13 @@
 import React from "react";
 import style from "../scss/Footer.module.scss";
 import { Link } from "react-router-dom";
+import baza from './assets/baza-logo.svg'
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
       <div className={style.first_block}>
-        <div className={style.desc}>
-          Уся інформація в нашому мобільному додатку надається добросовісно,
-          однак ми не робимо жодних заяв або гарантій будь-якого роду, явних або
-          неявних, щодо точності, адекватності, дійсності, реальності,
-          доступності або компетентності будь-якої інформації в мобільному
-          додатку. За жодних обставин ми не несемо жодної відповідальності перед
-          вами за будь-які збитки чи шкоду будь-якого роду, понесені в
-          результаті використання мобільного додатку або довіри до будь-якої
-          інформації, наданої в мобільному додатку. Ви використовуєте мобільний
-          додаток і будь-яку інформацію в цьому мобільному додатку виключно на
-          свій страх і ризик.
-        </div>
-
+        <img className={style.desc} src={baza} alt="Baza Trainee"></img>
         <div className={style.second_block}>
           <div className={style.about}>
             <h3> Про нас</h3>
