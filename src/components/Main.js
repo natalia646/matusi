7e7b202e2695196c73eb14528dd1ae34fb2a115c
natalia646 <nataxash46@gmail.com>
2023-10-09 20:03:45 +0300

    adaptive for heder and main

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,38 +7,44 @@ import phone2 from "../components/assets/phones/phone2.png";
 import google from "../components/assets/google_play.svg";
 import style from "../scss/Main.module.scss";
 
-
 const MainPart = () => {
   const dispatch = useDispatch();
 
   return (
     <div className={style.head_container}>
-      <div className={style.desc}>
-        <h4>Долучайся до спільноти</h4>
-        <h1>
-          Отримай можливість <br />
-          працювати, в той час, як{" "}
-          <span style={{ color: "#2000C1" }}>
-            {" "}
-            <br />
-            діти щасливі та у безпеці!
-          </span>
-        </h1>
-        <p>
-          Ми допоможемо тобі знайти поруч мам, з якими можна разом доглядати за
-          вашими дітьми. Об’єднайте дітей у дитячі групи та доглядайте за ними
-          по черзі у вільний час.
-        </p>
-        <div className={style.buttons}>
-          <Download />
-          <button
-            className={style.more}
-            onClick={() => dispatch(setDetails(true))}
-          >
-            Дивитися більше
-          </button>
+      <div className={style.back_mobile}>
+        <div className={style.desc}>
+
+          <h4>Долучайся до спільноти</h4>
+
+          <h1>
+            Отримай можливість <br />
+            працювати, в той час, як
+            <span style={{ color: "#2000C1" }}>
+              <br />
+              діти щасливі та у безпеці!
+            </span>
+          </h1>
+          
+          <h5>Долучайся до спільноти</h5>
+
+          <p>
+            Ми допоможемо тобі знайти поруч мам, з якими можна разом доглядати
+            за вашими дітьми. Об’єднайте дітей у дитячі групи та доглядайте за
+            ними по черзі у вільний час.
+          </p>
+          <div className={style.buttons}>
+            <Download />
+            <button
+              className={style.more}
+              onClick={() => dispatch(setDetails(true))}
+            >
+              Дивитися більше
+            </button>
+          </div>
         </div>
       </div>
+
       <div className={style.images}>
         <div className={style.google_play}>
           <img src={google} alt="Google Play"></img>
