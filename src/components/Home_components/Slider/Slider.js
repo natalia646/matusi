b83cab27462647eab5997a0b0f8b9f943f9f6bfb
natalia646 <nataxash46@gmail.com>
2023-10-20 11:00:09 +0300

    fix

--- src/components/Home_components/Slider/Slider.js
+++ src/components/Home_components/Slider/Slider.js
@@ -31,17 +31,17 @@ import image12 from "../../assets/slides/img12.png";
 
 const images = [
   image1,
+  image4,
   image2,
   image3,
-  image4,
-  image5,
   image6,
   image7,
   image8,
   image9,
   image10,
   image11,
-  image12
+  image12,
+  image5,
 ];
 
 const Slider = () => {
