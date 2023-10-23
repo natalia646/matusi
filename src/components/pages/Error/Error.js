cff635cc991587b55b0033c97e91e8867d2ae537
natalia646 <nataxash46@gmail.com>
2023-10-23 13:12:24 +0300

    fix error

--- src/components/pages/Error/Error.js
+++ src/components/pages/Error/Error.js
@@ -5,22 +5,13 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <main className={style.error}>
+    <div className={style.error}>
       <img src={error} alt="error"></img>
 
       <Link to="/">
         <button className={style.bt_desctop}>Головна</button>
       </Link>
-
-      <div className={style.mobile}>
-          <h2> Уупс!</h2>
-          <h2> Сторінку не знайдено.</h2>
-          <Link to="/">
-            <button className={style.bt_mobile}>Головна</button>
-          </Link>
-        <div className={style.fix}></div>
-      </div>
-    </main>
+    </div>
   );
 };
 
