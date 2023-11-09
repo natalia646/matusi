6e2bdf05f73d4cfe8e4991e6e5c21d11804a8d35
natalia646 <nataxash46@gmail.com>
2023-11-09 10:02:23 +0200

    fix swiper

--- src/components/Home_components/Slider/Slider.js
+++ src/components/Home_components/Slider/Slider.js
@@ -11,39 +11,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Slider.css";
-
-// import image1 from "https://ibb.co/b6V1Zvn";
-// import image2 from "../../assets/slides/img2.png";
-// import image3 from "../../assets/slides/img3.png";
-// import image4 from "../../assets/slides/img4.png";
-// import image5 from "../../assets/slides/img5.png";
-// import image6 from "../../assets/slides/img6.png";
-// import image7 from "../../assets/slides/img7.png";
-// import image8 from "../../assets/slides/img8.png";
-// import image9 from "../../assets/slides/img9.png";
-// import image10 from "../../assets/slides/img10.png";
-// import image11 from "../../assets/slides/img11.png";
-// import image12 from "../../assets/slides/img12.png";
-// import image13 from "../../assets/slides/img13.png";
-// import image14 from "../../assets/slides/img14.png";
-// import image15 from "../../assets/slides/img15.png";
-
-// https://ibb.co/b6V1Zvn
-// https://ibb.co/bbwg2pR
-// https://ibb.co/mbH9gYv
-// https://ibb.co/SympRwV
-// https://ibb.co/H7c21Qr
-// https://ibb.co/NYCPb28
-// https://ibb.co/g7T1wb0
-// https://ibb.co/GRtcVZ4
-// https://ibb.co/4j7M4zD
-// https://ibb.co/thXNYLq
-// https://ibb.co/kHqYCMb
-// https://ibb.co/QKytGdN
-// https://ibb.co/Tm7Pydr
-// https://ibb.co/6XX0Pjp
-// https://ibb.co/cb866wX
-
+import axios from "axios";
 
 // const images = [
 //   image13,
@@ -63,51 +31,74 @@ import "./Slider.css";
 //   image1,
 // ];
 
+// const images = [
+//   "https://i.ibb.co/JxpvvqF/img15.png",
+//   "https://i.ibb.co/mNN5qfY/img14.png",
+//   "https://i.ibb.co/9s1yPSG/img13.png",
+//   "https://i.ibb.co/NycbzSj/img12.png",
+//   "https://i.ibb.co/f1rmwQ6/img11.png",
+//   "https://i.ibb.co/VNmhMwg/img10.png",
+//   "https://i.ibb.co/fC2trfK/img9.png",
+//   "https://i.ibb.co/qpWkRH3/img8.png",
+//   "https://i.ibb.co/n0c2fqX/img7.png",
+//   "https://i.ibb.co/s3RGz90/img6.png",
+//   "https://i.ibb.co/tB0hk5b/img5.png",
+//   "https://i.ibb.co/PjW8wG1/img4.png",
+//   "https://i.ibb.co/8NjrtRz/img3.png",
+//   "https://i.ibb.co/RjrpBL4/img2.png",
+//   "https://i.ibb.co/QJV9RHG/img1.png",
+// ];
+
+
 const Slider = () => {
-const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    setIsLoading(false);
+    axios
+      .get("https://64ca66e8700d50e3c704da5c.mockapi.io/api/va/posts")
+      .then(({ data }) => setImages(data))
+      .catch((error) => console.log(error))
+      .finally(setIsLoading(true));
+  }, []);
 
-
+  if (!isLoading) {
+    return <div>Загрузка...</div>;
+  }
   return (
     <div className="container">
       <h2>Інтерфейс застосунку</h2>
       <Swiper
-         effect={"coverflow"}
-         grabCursor={true}
-         centeredSlides={true}
-         loop={true}
-         slidesPerView={3}
-         coverflowEffect={{
-           rotate: 0,
-           stretch: 0,
-           depth: 100,
-           modifier: 2.5,
-         }}
-         pagination={{ el: ".swiper-pagination", clickable: true }}
-         navigation={{
-           nextEl: ".swiper-button-next",
-           prevEl: ".swiper-button-prev",
-           clickable: true,
-         }}
-         freeMode={{ enabled: true, momentum: true, minimumVelocity: 0.05 }}
-         modules={[EffectCoverflow, Pagination, Navigation, FreeMode]}
-         className="swiper-container"
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        freeMode={{ enabled: true, momentum: true, minimumVelocity: 0.05 }}
+        modules={[EffectCoverflow, Pagination, Navigation, FreeMode]}
+        className="swiper-container"
       >
-        {images.map((image, i) => {
+        {images.map((image) => {
           return (
-            <SwiperSlide key={i}>
-              <img className="image" src={image} alt="slide_image" />
+            <SwiperSlide key={image.id}>
+              <img className="image" src={image.imageUrl} alt="slide_image" />
             </SwiperSlide>
           );
         })}
-
         <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
           <div className="swiper-pagination"></div>
         </div>
       </Swiper>
