96136d5169720e4fba800c39ef6944a937a82679
natalia646 <nataxash46@gmail.com>
2023-11-20 16:46:35 +0200

    Revert "fix"
    This reverts commit 4ebf8294fbf91a2ea5ea3db64013dd06e41f0897.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,20 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Context } from "../../context";
 import { Link, useLocation } from "react-router-dom";
-// import line from "../assets/icons/line.svg";
+import line from "../assets/icons/line.svg";
 import logo from "../assets/logo/logo.svg";
 import open from "../assets/icons/open-menu.svg";
 import close from "../assets/icons/close.svg";
 import style from "./Header.module.scss";
 import Download from "../Home_components/Download/Download";
-import styled from "styled-components";
-import { device } from "../../device";
 
-const links = [
-  {
-    link: "/",
-    nameLink: "Про застосунок",
-  },
-  {
-    link: "/",
-    nameLink: "Відгуки",
-  },
-  {
-    link: "/terms",
-    nameLink: "Умови користування",
-  },
-  {
-    link: "/contact",
-    nameLink: "Контакти",
-  },
+const links = ["/", "/", "/terms", "/contact"];
+const nameLink = [
+  "Про застосунок",
+  "Відгуки",
+  "Умови користування",
+  "Контакти",
 ];
-
 const linksMobile = [
   {
     link: "/refusal",
@@ -51,40 +37,44 @@ const Header = () => {
       setActiveIndex(links.indexOf(url));
     }
   });
+
   const getActiveID = (i) => {
     setActiveIndex(i);
     setIsMobile(true);
   };
 
   return (
-    <HeaderBlock>
-      <Container>
+
+    <header>
+      <div className={style.container}>
         <Link to="/">
-          <Logo src={logo} alt="logo"></Logo>
+          <img src={logo} alt="logo"></img>
         </Link>
-        <BurgerMenu
+
+        <img
           onClick={() => setIsMobile(!isMobile)}
           src={isMobile ? open : close}
-          alt="menu"
-        ></BurgerMenu>
+          alt="menu"></img>
+
 
         <div className={`${isMobile ? style.open : style.close}`}>
           {links.map((item, i) => {
             return (
               <Link
-                to={item.link}
+                to={item}
                 key={i}
                 onClick={() => getActiveID(i)}
                 className={`${style.link} ${
                   i === activeIndex ? style.active : ""
                 } `}
               >
-                {/* <img
+                <img
                   src={line}
                   alt="line"
                   className={i === activeIndex ? style.line : style.not_line}
-                ></img> */}
+                ></img>
                 {item.nameLink}
+                {nameLink[i]}
               </Link>
             );
           })}
@@ -108,29 +98,9 @@ const Header = () => {
             ))}
           </span>
         </div>
-      </Container>
-    </HeaderBlock>
+      </div>
+    </header>
   );
 };
 
 export default Header;
-
-const HeaderBlock = styled.header`
-  margin-left: 6.25rem;
-  margin-right: 6.25rem;
-`;
-const Container = styled.div`
-  margin-top: 25px;
-  margin-bottom: 25px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const Logo = styled.img`
-  width: 2.75rem;
-  height: 2.75rem;
-`;
-const BurgerMenu = styled.img`
-  display: none;
-`;
-
