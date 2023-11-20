31f116d8c1061c31a2134d7fad13bf56e8f0d32e
natalia646 <nataxash46@gmail.com>
2023-11-20 17:45:47 +0200

    desctop for header with styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,21 +9,32 @@ import style from "./Header.module.scss";
 import Download from "../Home_components/Download/Download";
 import styled from "styled-components";
 
-const links = ["/", "/", "/terms", "/contact"];
-const nameLink = [
-  "Про застосунок",
-  "Відгуки",
-  "Умови користування",
-  "Контакти",
+const links = [
+  {
+    link: "/",
+    nameLink: "Про застосунок",
+  },
+  {
+    link: "/",
+    nameLink: "Відгуки",
+  },
+  {
+    link: "/terms",
+    nameLink: "Умови користування",
+  },
+  {
+    link: "/contact",
+    nameLink: "Контакти",
+  },
 ];
 const linksMobile = [
   {
     link: "/refusal",
-    label: "Відмова від відповідальності",
+    nameLink: "Відмова від відповідальності",
   },
   {
     link: "/privacy",
-    label: "Політика конфіденційності",
+    nameLink: "Політика конфіденційності",
   },
 ];
 
@@ -34,13 +45,13 @@ const Header = () => {
 
   useEffect(() => {
     if (location.pathname) {
-      const url = links.find((link) => link === location.pathname);
+      const url = links.find(({ link }) => link === location.pathname);
       setActiveIndex(links.indexOf(url));
     }
   });
 
-  const getActiveID = (i) => {
-    setActiveIndex(i);
+  const getActiveId = (index) => {
+    setActiveIndex(index);
     setIsMobile(true);
   };
 
@@ -49,9 +60,22 @@ const Header = () => {
       <Container>
         <Link to={"/"}>
           <Logo src={logo} alt="logo"></Logo>
-
-
         </Link>
+
+        <DescLinks>
+          {links.map((item, i) => (
+            <Link
+              key={i}
+              to={item.link}
+              onClick={() => getActiveId(i)}
+              className={i === activeIndex ? "activeLink" : ""}
+            >
+              <span className={i === activeIndex ? "activeSpan" : ""}></span>
+              {item.nameLink}
+            </Link>
+          ))}
+          <Download />
+        </DescLinks>
       </Container>
     </HeaderBlock>
   );
@@ -60,16 +84,41 @@ const Header = () => {
 export default Header;
 
 const HeaderBlock = styled.header`
-  margin: 2rem 0;
+  margin: 2.12rem 0 2.44rem 0;
 `;
 const Container = styled.div`
   margin-left: 6.25rem;
   margin-right: 6.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 const Logo = styled.img`
   width: 2.75rem;
   height: 2.75rem;
 `;
 const DescLinks = styled.div`
-
-`
+  a {
+    text-decoration: none;
+    color: #000;
+    font-size: 1.25rem;
+    font-weight: 400;
+    margin-left: 2.63rem;
+  }
+  .activeLink {
+    font-weight: 700;
+  }
+  .activeSpan {
+    width: 2.5rem;
+    height: 0.25rem;
+    border-radius: 0.25rem;
+    background-color: var(--orange-color);
+    display: inline-block;
+    position: relative;
+    top: 0.65rem;
+    left: 2.4rem;
+  }
+  span {
+    display: none;
+  }
+`;
